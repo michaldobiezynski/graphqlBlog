9b8d3816f4cd649c1abb219be000616120310d87
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-26 21:04:25 +0000

    returning data in the resolver function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,16 @@
 const graphql = require("graphql");
+const _ = require("lodash");
+
+//dummy data
+let usersData = [
+  { id: "1", name: "Bob", age: 36 },
+  { id: "2", name: "Angela", age: 16 },
+  { id: "3", name: "Cebula", age: 34 },
+  { id: "4", name: "Andrzej", age: 50 },
+  { id: "5", name: "Greg", age: 43 },
+  { id: "6", name: "Wika", age: 25 },
+  { id: "7", name: "Bogdan", age: 77 },
+];
 
 const {
   GraphQLObjectType,
@@ -29,6 +41,9 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
 
       resolve(parent, args) {
+        return _.find(usersData, {
+          id: args.id,
+        });
         // we resolve with data
         // get and return data from a datasource
       },
