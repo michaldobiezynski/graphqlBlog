6bb715ee6fff5cc83203c3173db6789e07c936b2
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-03-05 21:39:51 +0000

    showing posts on page

--- client/blog-graphql/src/components/DisplayPosts.js
+++ client/blog-graphql/src/components/DisplayPosts.js
@@ -1,15 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { listPosts } from "../graphql/queries";
 import { API, graphqlOperation } from "aws-amplify";
 
 export const DisplayPosts = () => {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     const getPosts = async () => {
       const result = await API.graphql(graphqlOperation(listPosts));
-      console.log("All posts: ", JSON.stringify(result.data.listPosts.items));
+
+      setPosts(result.data.listPosts.items);
+
+      //   console.log("All posts: ", result.data.listPosts.items);
     };
     getPosts();
   }, []);
 
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <ul>
+        {posts.map((_post) => {
+          return (
+            <div className="posts" key={_post.id}>
+              <h1 key={_post.id}>{_post.postTitle}</h1>
+              <span>
+                {`Wrote by: `} {_post.postOwnerUsername}
+                <br />
+                <time>
+                  {" on "} {new Date(_post.createdAt).toDateString()}
+                </time>
+              </span>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
