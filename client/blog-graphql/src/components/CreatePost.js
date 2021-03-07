1837e57c92448ca52ba0fa8de6ae6e283db781f2
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-03-07 11:13:15 +0000

    sendpint posts to backend aws dynamoDB

--- client/blog-graphql/src/components/CreatePost.js
+++ client/blog-graphql/src/components/CreatePost.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { createPost } from "../graphql/mutations";
+import { API, graphqlOperation } from "aws-amplify";
 
 export const CreatePost = () => {
   const [postOwnerID, setPostOwnerID] = useState("");
@@ -6,16 +8,26 @@ export const CreatePost = () => {
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
 
+  const handleChangePost = (event) => {
+    event.target.name = event.target.value;
+  };
+
   const handleAddPost = async (event) => {
     event.preventDefault();
 
     const input = {
-      postOwnerId: postOwnerID,
-      postOwnerUserName: postOwnerUserName,
+      // postOwnerId: postOwnerID,
+      postOwnerId: "pa817623",
+      // postOwnerUserName: postOwnerUserName,
+      postOwnerUsername: "Michal",
       postTitle: postTitle,
       postBody: postBody,
       createdAt: new Date().toISOString(),
     };
+
+    await API.graphql(graphqlOperation(createPost, { input }));
+    setPostTitle("");
+    setPostBody("");
   };
 
   useEffect(() => {
@@ -29,6 +41,10 @@ export const CreatePost = () => {
         type="text"
         placeholder="Title"
         name="postTitle"
+        value={postTitle}
+        onChange={(event) => {
+          setPostTitle(event.target.value);
+        }}
       />
       <textarea
         type="text"
@@ -37,6 +53,10 @@ export const CreatePost = () => {
         cols="40"
         required
         placeholder="New Blog Post"
+        value={postBody}
+        onChange={(event) => {
+          setPostBody(event.target.value);
+        }}
       />
       <input type="submit" className="btn" style={{ fontSize: "19px" }} />
     </form>
